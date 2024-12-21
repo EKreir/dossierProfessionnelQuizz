44a58040397ccf8915fea5da3ee144590a853f51
EKreir <ekreir97@gmail.com>
2024-12-21 09:10:18 +0100

    add questions variable

---
+++ script.js
@@ -0,0 +1,20 @@
+const questions = [
+    {
+        question: "Qui est le premier sensei de Naruto ?",
+        type: "single", // Choix unique
+        answers: ["Kakashi", "Iruka", "Gaï", "Hiruzen"],
+        correctAnswer: "Iruka"
+    },
+    {
+        question: "Qui étaient les deux autres équipiers de Naruto dans l'équipe 7 ?",
+        type: "multiple", // Choix multiples
+        answers: ["Sakura", "Tenten", "Gaara", "Sasuke"],
+        correctAnswer: ["Sasuke", "Sakura"]
+    },
+    {
+        question: "Naruto est le fisl du quatrième Hokage",
+        type: "true/false", // Vrai/Faux
+        answers: ["Vrai", "Faux"],
+        correctAnswer: "Vrai"
+    }
+];
